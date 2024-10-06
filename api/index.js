970be261aec128bd0cb8d71e66daa76656cf5d7e
cyb3ritic <cyb3ritic@gmail.com>
2024-10-06 19:05:52 +0530

    connect database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express'; // ES module syntax
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log('MongoDb is connected');
+    }
+).catch(err => {
+    console.log(err)
+});
 
 const app = express(); // app is initialized by calling express()
 

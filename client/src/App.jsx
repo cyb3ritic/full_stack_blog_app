71d95d29405056fef044c455cb60dccad847627a
cyb3ritic <cyb3ritic@gmail.com>
2024-10-06 15:08:13 +0530

    create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,15 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
 
+      <Header/>
+
+      <Routes>
 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -18,9 +21,6 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-
-
-
 
       </Routes>
     

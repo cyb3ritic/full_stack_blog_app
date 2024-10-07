30115e267d587f4e69364bd08aecb35d15b6dd91
cyb3ritic <cyb3ritic@gmail.com>
2024-10-07 18:11:23 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'; // ES module syntax
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000!!");
 });
 
-
-
+app.use(express.json());
 app.use('/api/user',userRoute)
+
+app.use('/api/auth',authRoute)
